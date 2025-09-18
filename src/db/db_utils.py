da693aeb60fe7a61a2fc9ad89f6625ed9d5d9268
realover82 <59042930+realover82@users.noreply.github.com>
2025-09-19 06:31:59 +0900

    Add files via upload

---
+++ src/db/db_utils.py
@@ -0,0 +1,28 @@
+import streamlit as st
+import sqlite3
+import pandas as pd
+
+@st.cache_resource
+def get_connection():
+    """
+    SQLite 데이터베이스에 연결합니다.
+    """
+    try:
+        db_path = "./SJ_TM2360E_v2.sqlite3"
+        conn = sqlite3.connect(db_path, check_same_thread=False)
+        return conn
+    except Exception as e:
+        st.error(f"데이터베이스 연결에 실패했습니다: {e}")
+        return None
+
+def read_data_from_db(conn, table_name):
+    """
+    데이터베이스에서 지정된 테이블의 데이터를 읽어 DataFrame으로 반환합니다.
+    """
+    try:
+        query = f"SELECT * FROM {table_name}"
+        df = pd.read_sql_query(query, conn)
+        return df
+    except Exception as e:
+        st.error(f"테이블 '{table_name}'에서 데이터를 불러오는 중 오류가 발생했습니다: {e}")
+        return None
